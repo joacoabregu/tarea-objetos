da8e4ce83749b0cfa116f492ebaa01510406524e
Joaquin Abregu <abregujoaquin@gmail.com>
2021-05-19 20:39:26 -0300

    notas test

--- main.mjs
+++ main.mjs
@@ -1,4 +1,5 @@
 import Autor from "./autor.mjs";
+import Notas from "./notas.mjs";
 
 let borges = new Autor({
     nombre: "Jorge Luis Borges",
@@ -6,6 +7,12 @@ let borges = new Autor({
     libros: ['Historia universal de la infamia', 'Ficciones', 'El Aleph'],
     generos: ['Poesía', 'Cuento', 'Ensayo'],
     mail: 'jorgitoborges@borges.com' 
+});
+
+let notaBorges = new Notas({
+    nota: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis sapien, hendrerit ut arcu eu, gravida porttitor felis. Cras libero elit, imperdiet eu ipsum in, feugiat vestibulum sem. Suspendisse potenti. Pellentesque elementum quam quis ex luctus tempus sed id est. Suspendisse vitae magna venenatis, tristique velit a, convallis sem. Fusce tristique risus a vulputate pulvinar. Phasellus maximus neque a sagittis rutrum. Aliquam erat volutpat. Vestibulum nec libero a purus dapibus rutrum. Pellentesque finibus ultricies laoreet. Nulla fringilla hendrerit ante ac venenatis. Maecenas pulvinar lacinia accumsan. Nulla libero dolor, molestie aliquet sagittis nec, consequat eu mauris. Pellentesque dui sem, bibendum dignissim venenatis a, placerat quis libero. Ut non sollicitudin eros. Etiam tempus, sapien nec egestas dignissim, ipsum quam tempor nunc, vel fermentum sapien sapien nec lacus. Cras eu felis massa. Nullam volutpat iaculis turpis fringilla commodo. Quisque ac dui eu augue pharetra iaculis. Proin maximus lacus at facilisis condimentum. Nam ultrices consectetur eros, eu pellentesque diam tempor vel. In sodales tincidunt est sollicitudin viverra. ',
+    fecha: '19/5/1955',
+    tag: ['nuevo', 'poesia']
 });
 
 // console.log(borges);
@@ -42,3 +49,25 @@ try {
 }
 
  console.log(borges); */
+
+ // console.log(notaBorges);
+// console.log(notaBorges.getNota());
+// console.log(notaBorges.getFecha());
+// console.log(notaBorges.getTag());
+
+/* try {
+    notaBorges.setNota('Probando 123')
+} catch (error) {
+    console.error(error.message)
+}
+try {
+    notaBorges.setFecha('12/12/1960')
+} catch (error) {
+    console.error(error.message)
+}
+try {
+    notaBorges.setTag(['Probando 123', 'Probando 456'])
+} catch (error) {
+    console.error(error.message)
+}
+console.log(notaBorges); */
