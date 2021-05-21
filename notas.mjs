1cfe305e1aebb041a866e94bbc781bb1306d6bbc
Joaquin Abregu <abregujoaquin@gmail.com>
2021-05-21 12:19:35 -0300

    agregar propiedad autor a clase notas

--- notas.mjs
+++ notas.mjs
@@ -1,14 +1,18 @@
 export default class Notas {
     constructor(data = {}) {
+        this.autor = data.autor || '';
         this.nota = data.nota || '';
         this.fecha = data.fecha || '';
         this.tag = data.tag || [''];
     }
-
+    getAutor() { return this.autor; };
     getNota() { return this.nota; };
     getFecha() { return this.fecha; };
     getTag() { return this.tag};
 
+    setAutor(_autor) {
+        this.autor = _autor;
+    }
     setNota(_nota) {
         this.nota = _nota;
     }
